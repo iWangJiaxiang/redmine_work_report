31d28a3d114943071593e65157756dec55b9d572
王嘉祥 <iwangjiaxiang@outlook.com>
2020-03-17 14:49:33 +0800

    init

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_work_report do
+  name 'Redmine Work Report plugin'
+  author 'Wang Jiaxiang <mail@wjx.me>'
+  description 'This is a plugin for Redmine, which allow users to write work reports for each projects periodically.'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
